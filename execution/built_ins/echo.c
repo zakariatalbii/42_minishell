bef32a70fb6a34b0fb70b6f53bf6b8840d6512e4
wafae-nid <wafaenido8@gmail.com>
2025-07-04 16:35:11 +0100

    echo - echo -nnnn - works pleasetest it and let me know if there is anything wrong

--- execution/built_ins/echo.c
+++ execution/built_ins/echo.c
@@ -13,15 +13,82 @@
 
 #include "../../minishell.h"
 
+// static int identify_the_arg(char **str, int *i)
+// {
+//     int j ;
+
+//     j = 0;
+
+//    while(str[*i])
+//     {
+//         if (str[*i][0] == '-')
+//         {
+
+//             j = 1;
+//             while(str[*i][j])
+//             {
+//                 if(str[*i][j] != 'n')
+//                 {
+//                     return(0);
+//                 }
+//                 j++;
+//             }
+//         }
+//         else 
+//             return(0);
+//         (*i)++;
+//     }
+//     return(1); 
+// }
+
+// void echo_execution(char **command, t_env_var **env_vars)
+// {
+//     int i;
+//     int new_line;
+
+//     new_line = 0;
+//     i = 1;
+//     if(!identify_the_arg((command), &i))
+//     {
+//         if(i != 1)//cause if i != 1 of course we did pass one valid argument   
+//             new_line = 1; 
+//         i = 1;
+//     }
+//     // else 
+//     // {
+//     //     if(!command[i])
+//     //         new_line = 1;
+//     // }
+//     while((command)[i])
+//     {
+//         printf("%s",(command)[i]);
+//         if((command)[i+1] != NULL)
+//             printf(" ");
+//         i++;
+//     }
+//     if(new_line == 0)
+//         printf("\n");
+//     *((*env_vars)->status) = 0;
+// }
+
+
 static int identify_the_arg(char **str, int *i)
 {
     int j ;
+    int index;
+
     j = 0;
+    index = 0;
+
 
    while(str[*i])
     {
         if (str[*i][0] == '-')
         {
+            if(!ft_strcmp(str[*i], "-"))
+            {
+                return(0);
+            }
             j = 1;
             while(str[*i][j])
             {
@@ -50,6 +117,10 @@ void echo_execution(char **command, t_env_var **env_vars)
         if(i != 1 )//cause if i != 1 of course we did pass one valid argument   
             new_line = 1; 
     }
+    else 
+    {
+        new_line = 1;
+    }
     while((command)[i])
     {
         printf("%s",(command)[i]);
@@ -61,4 +132,3 @@ void echo_execution(char **command, t_env_var **env_vars)
         printf("\n");
     *((*env_vars)->status) = 0;
 }
-
